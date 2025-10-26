1b2f4cf3366d1a3046b0785f0ce89ed992aa8c67
NNP-coder-rgb <nguyenphuc230607@gmail.com>
2025-10-26 15:43:31 +0700

    Update EX10.c
    Reversed the digits of the input number and printed them.

--- SESSION_6/EX10.c
+++ SESSION_6/EX10.c
@@ -6,10 +6,18 @@ int main (){
 	scanf("%d",&n);
 	
 	printf("Cac chu so cua %d la: ",n);
+	int temp = n;
+	int dao = 0;
 	while(n>0){
 		int chu_so = n % 10;
-		printf("%d ",chu_so);
+		dao = dao * 10 + chu_so;
 		n = n/10;
+	}
+	while(dao > 0){
+		int chu_so = dao % 10;
+		printf("%d ",chu_so);
+		dao /= 10;
 	}
 	return 0; 
 }
+
