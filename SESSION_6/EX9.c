f01f9129848d55c60eef90d04414cfc66a25465d
NNP-coder-rgb <nguyenphuc230607@gmail.com>
2025-10-29 20:29:20 +0700

    Update EX9.c
    Added a flag to check if numbers were entered before performing calculations.

--- SESSION_6/EX9.c
+++ SESSION_6/EX9.c
@@ -6,6 +6,7 @@ int main (){
 	int sum;
 	float average;
 	int min, max;
+	int da_nhap = 0;
 	
 	do{
 		printf("MENU:\n 1.Nhap ba so\n 2.Tong ba so\n 3.Trung binh song ba so\n 4.So lon nhat\n 5.So nho nhat\n 6.Thoat\n");
@@ -22,40 +23,33 @@ int main (){
 				
 				printf("Nhap vao so c:");
 				scanf("%d",&c);
+				da_nhap = 1;
 				break;
 			case 2:
-				printf("Nhap vao so a:");
-				scanf("%d",&a);
+				if(da_nhap == 0){
+					printf("Vui long nhap vao 3 so a,b,c:\n");
+				}else{
 				
-				printf("Nhap vao so b:");
-				scanf("%d",&b);
-				
-				printf("Nhap vao so c:");
-				scanf("%d",&c);
 				sum = a + b +c;
 				printf("Tong cua ba so la: %d\n",sum);
+				}
 				break;
+				
 			case 3:
-				printf("Nhap vao so a:");
-				scanf("%d",&a);
-				
-				printf("Nhap vao so b:");
-				scanf("%d",&b);
-				
-				printf("Nhap vao so c:");
-				scanf("%d",&c);
+				if(da_nhap == 0){
+					printf("Vui long nhap vao 3 so a,b,c:\n");
+				}else{
+					
 				average = (a + b + c)/3.0;
 				printf("Trung binh cua ba so la: %.2f",average);
+				}
 				break;
+				
 			case 4:
-				printf("Nhap vao so a:");
-				scanf("%d",&a);
-				
-				printf("Nhap vao so b:");
-				scanf("%d",&b);
-				
-				printf("Nhap vao so c:");
-				scanf("%d",&c);
+				if(da_nhap == 0){
+					printf("Vui long nhap vao 3 so a,b,c:\n");
+				}else{
+					
 				max = a;
 				if(b > max){			
 					max = b;
@@ -64,16 +58,14 @@ int main (){
 					max = c;
 				}
 				printf("So lon nhat la: %d\n",max);
+				}
 				break;
+				
 			case 5:
-				printf("Nhap vao so a:");
-				scanf("%d",&a);
-				
-				printf("Nhap vao so b:");
-				scanf("%d",&b);
-				
-				printf("Nhap vao so c:");
-				scanf("%d",&c);
+				if(da_nhap == 0){
+					printf("Vui long nhap vao 3 so a,b,c:\n");
+				}else{
+					
 				min = a;
 				if(b < min){
 					min = b;
@@ -82,7 +74,9 @@ int main (){
 					min = c;
 				}
 				printf("So nho nhat la: %d\n",min);
+				}
 				break;
+				
 			case 6:
 				printf("Tam biet!");
 				break;
